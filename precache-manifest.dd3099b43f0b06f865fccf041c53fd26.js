01a4ac55024b5b7d2f97dc051ce1cfe8b4a206bc
unknown <mietlinski.wojciech@gmail.com>
2020-07-07 23:56:21 +0200

    Updates

---
+++ precache-manifest.dd3099b43f0b06f865fccf041c53fd26.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba36b6ef7a53fb3064b20cac0a805152",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "de0274f835752dd9996c",
+    "url": "/portfolio-website/static/css/main.2284cdf2.chunk.css"
+  },
+  {
+    "revision": "8cd0d11e9d00fec8fb17",
+    "url": "/portfolio-website/static/js/2.8c4729ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-website/static/js/2.8c4729ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de0274f835752dd9996c",
+    "url": "/portfolio-website/static/js/main.1e1eb615.chunk.js"
+  },
+  {
+    "revision": "99cb685b86efd0f580da",
+    "url": "/portfolio-website/static/js/runtime-main.c16362e1.js"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/portfolio-website/static/media/Raleway-Regular.9942588a.ttf"
+  },
+  {
+    "revision": "1f14ea36322136095fb685aee3582b4b",
+    "url": "/portfolio-website/static/media/arrow.1f14ea36.svg"
+  },
+  {
+    "revision": "6971ddc761cc35ba918be0a69dfbcf50",
+    "url": "/portfolio-website/static/media/css-5.6971ddc7.svg"
+  },
+  {
+    "revision": "f0361c5e9bebc1c5647523db385641cf",
+    "url": "/portfolio-website/static/media/html5.f0361c5e.svg"
+  },
+  {
+    "revision": "891a71b9a98f1d58491e1b06c4d356a2",
+    "url": "/portfolio-website/static/media/js.891a71b9.svg"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/portfolio-website/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "b337a2fb0b1289e6757e632dc766ff6b",
+    "url": "/portfolio-website/static/media/nodejs-icon.b337a2fb.svg"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/portfolio-website/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/portfolio-website/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/portfolio-website/static/media/sass-1.b446ca5a.svg"
+  }
+]);
