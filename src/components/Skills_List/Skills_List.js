eaae0942e9a8fb7dda45a74d23afd2edadc38159
unknown <mietlinski.wojtek@gmail.com>
2020-09-03 21:24:51 +0200

    added new skills

--- src/components/Skills_List/Skills_List.js
+++ src/components/Skills_List/Skills_List.js
@@ -1,62 +1,76 @@
-import React from "react";
-import "./Skill_List.css";
+import React from 'react';
+import './Skill_List.css';
 
-import { ReactComponent as HtmlIcon } from "../icons/html5.svg";
-import { ReactComponent as CssIcon } from "../icons/css-5.svg";
-import { ReactComponent as JsIcon } from "../icons/js.svg";
-import { ReactComponent as MaterialIcon } from "../icons/material-ui-1.svg";
-import { ReactComponent as SassIcon } from "../icons/sass-1.svg";
-import { ReactComponent as ReactIcon } from "../icons/react.svg";
-import { ReactComponent as NodejsIcon } from "../icons/nodejs-icon.svg";
-import { ReactComponent as PostgresqlIcon } from "../icons/postgresql.svg";
+import { ReactComponent as HtmlIcon } from '../icons/html5.svg';
+import { ReactComponent as CssIcon } from '../icons/css-5.svg';
+import { ReactComponent as JsIcon } from '../icons/js.svg';
+import { ReactComponent as MaterialIcon } from '../icons/material-ui-1.svg';
+import { ReactComponent as SassIcon } from '../icons/sass-1.svg';
+import { ReactComponent as ReactIcon } from '../icons/react.svg';
+import { ReactComponent as NodejsIcon } from '../icons/nodejs-icon.svg';
+import { ReactComponent as PostgresqlIcon } from '../icons/postgresql.svg';
+import { ReactComponent as ReduxIcon } from '../icons/redux.svg';
+import { ReactComponent as FirebaseIcon } from '../icons/firebase.svg';
 
 const skill_list = () => {
   return (
-    <div className="container_skills">
-      <div className="card_skills">
-        <HtmlIcon className="icon" />
+    <div className='container_skills'>
+      <div className='card_skills'>
+        <HtmlIcon className='icon' />
         <div>
           <b>Html 5</b>
         </div>
       </div>
-      <div className="card_skills">
-        <CssIcon className="icon" />
+      <div className='card_skills'>
+        <CssIcon className='icon' />
         <div>
           <b>Css 3</b>
         </div>
       </div>
-      <div className="card_skills">
-        <JsIcon className="icon" />
+      <div className='card_skills'>
+        <JsIcon className='icon' />
         <div>
           <b>JavaScript</b>
         </div>
       </div>
-      <div className="card_skills">
-        <SassIcon className="icon" />
+      <div className='card_skills'>
+        <SassIcon className='icon' />
         <div>
           <b>Sass</b>
         </div>
       </div>
-      <div className="card_skills">
-        <MaterialIcon className="icon" />
+      <div className='card_skills'>
+        <MaterialIcon className='icon' />
         <div>
           <b>Material UI</b>
         </div>
       </div>
-      <div className="card_skills">
-        <ReactIcon className="icon" />
+      <div className='card_skills'>
+        <ReactIcon className='icon' />
         <div>
-          <b>react</b>
+          <b>React</b>
         </div>
       </div>
-      <div className="card_skills">
-        <NodejsIcon className="icon" />
+      <div className='card_skills'>
+        <ReduxIcon className='icon' />
+        <div>
+          <b>Redux</b>
+        </div>
+      </div>
+      <div className='card_skills'>
+        <FirebaseIcon className='icon' />
+        <div>
+          <b>Firebase</b>
+        </div>
+      </div>
+      <div className='card_skills'>
+        <NodejsIcon className='icon' />
         <div>
           <b>Node.js</b>
         </div>
       </div>
-      <div className="card_skills">
-        <PostgresqlIcon className="icon" />
+      <div className='card_skills'>
+        <PostgresqlIcon className='icon' />
         <div>
           <b>PostgreSQL</b>
         </div>
